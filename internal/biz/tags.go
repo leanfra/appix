9438c99dc714b9fc2654d8afcfa62559cad0db4b
aggong <uniwolf@163.com>
2024-12-01 12:55:00 +0800

    feature. tags code on usecase level.

--- internal/biz/tags.go
+++ internal/biz/tags.go
@@ -2,6 +2,7 @@ package biz
 
 import (
 	"context"
+	"fmt"
 )
 
 type TagsRepo interface {
@@ -22,27 +23,55 @@ func NewTagsUsecase(repo TagsRepo) *TagsUsecase {
 	}
 }
 
+func (s *TagsUsecase) validateTags(isNew bool, tags []Tag) error {
+	for _, t := range tags {
+		if err := t.Validate(isNew); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // CreateTags is
 func (s *TagsUsecase) CreateTags(ctx context.Context, tags []Tag) error {
+	// validate tags
+	if err := s.validateTags(true, tags); err != nil {
+		return err
+	}
+
 	return s.repo.CreateTags(ctx, tags)
 }
 
 // UpdateTags is
 func (s *TagsUsecase) UpdateTags(ctx context.Context, tags []Tag) error {
+
+	if err := s.validateTags(false, tags); err != nil {
+		return err
+	}
 	return s.repo.UpdateTags(ctx, tags)
 }
 
 // DeleteTags is
 func (s *TagsUsecase) DeleteTags(ctx context.Context, ids []string) error {
+
+	if len(ids) == 0 {
+		return fmt.Errorf("EmptyIds")
+	}
 	return s.repo.DeleteTags(ctx, ids)
 }
 
 // GetTags is
 func (s *TagsUsecase) GetTags(ctx context.Context, id string) (Tag, error) {
+	if len(id) == 0 {
+		return Tag{}, fmt.Errorf("EmptyId")
+	}
 	return s.repo.GetTags(ctx, id)
 }
 
 // ListTags is
 func (s *TagsUsecase) ListTags(ctx context.Context, filter *ListTagsFilter) ([]Tag, error) {
+	if err := filter.Validate(); err != nil {
+		return nil, err
+	}
 	return s.repo.ListTags(ctx, filter)
 }
--- internal/biz/tags_models.go
+++ internal/biz/tags_models.go
@@ -1,5 +1,7 @@
 package biz
 
+import "fmt"
+
 type Tag struct {
 	Id    string
 	Key   string
@@ -15,4 +17,35 @@ type ListTagsFilter struct {
 	Page     int64
 	PageSize int64
 	Filters  []TagFilter
+}
+
+func (t *Tag) Validate(isNew bool) error {
+	if len(t.Key) == 0 || len(t.Value) == 0 {
+		return fmt.Errorf("InvalidKeyValue")
+	}
+	if !isNew {
+		if len(t.Id) == 0 {
+			return fmt.Errorf("InvalidId")
+		}
+	}
+	return nil
+}
+
+func (f *TagFilter) Validate() error {
+	if len(f.Key) == 0 || len(f.Value) == 0 {
+		return fmt.Errorf("InvalidTagFilterKeyValue")
+	}
+	return nil
+}
+
+func (lf *ListTagsFilter) Validate() error {
+	if lf.Page < 0 || lf.PageSize < 0 {
+		return fmt.Errorf("ListTagFilterInvliadPagePagesize")
+	}
+	for _, f := range lf.Filters {
+		if err := f.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
