c71f2c68d470ab514708dbd2c8fa0d36544ded31
aggong <uniwolf@163.com>
2024-12-01 13:03:34 +0800

    feature. tags code interface on data level

--- internal/data/tags.go
+++ internal/data/tags.go
@@ -1,14 +1,13 @@
 package data
 
 import (
+	"appix/internal/biz"
 	"context"
-	"errors"
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
-    "gorm.io/gorm"
-
-	//  TODO: modify project name
-	// biz "appix/internal/biz"
+	"gorm.io/gorm"
 )
 
 type TagsRepoImpl struct {
@@ -20,18 +19,18 @@ func NewTagsRepoImpl(dsn string) (*TagsRepoImpl, error) {
 	var _db *gorm.DB
 	var err error
 	if len(dsn) > 0 {
-        if dsn[:5] == "mysql" {
-            // 连接 MySQL 数据库
-            _db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-        } else if dsn[:6] == "sqlite" {
-            // 连接 SQLite 数据库
-            _db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{})
-        } else {
-            return nil, errors.New("Unsupported database DSN format")
-        }
-    } else {
-        return nil, errors.New("DSN is not provided")
-    }
+		if dsn[:5] == "mysql" {
+			// 连接 MySQL 数据库
+			_db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		} else if dsn[:6] == "sqlite" {
+			// 连接 SQLite 数据库
+			_db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+		} else {
+			return nil, fmt.Errorf("UnsupportedDatabaseDSN")
+		}
+	} else {
+		return nil, fmt.Errorf("EmptyDSN")
+	}
 
 	if err != nil {
 		return nil, err
@@ -41,34 +40,38 @@ func NewTagsRepoImpl(dsn string) (*TagsRepoImpl, error) {
 	}, nil
 }
 
-
 // CreateTags is
-func (d *TagsRepoImpl) CreateTags(ctx context.Context) error {
+func (d *TagsRepoImpl) CreateTags(ctx context.Context, tags []biz.Tag) error {
 	// TODO database operations
 
 	return nil
 }
+
 // UpdateTags is
-func (d *TagsRepoImpl) UpdateTags(ctx context.Context) error {
+func (d *TagsRepoImpl) UpdateTags(ctx context.Context, tags []biz.Tag) error {
 	// TODO database operations
 
 	return nil
 }
+
 // DeleteTags is
-func (d *TagsRepoImpl) DeleteTags(ctx context.Context) error {
+func (d *TagsRepoImpl) DeleteTags(ctx context.Context, ids []string) error {
 	// TODO database operations
 
 	return nil
 }
+
 // GetTags is
-func (d *TagsRepoImpl) GetTags(ctx context.Context) error {
+func (d *TagsRepoImpl) GetTags(ctx context.Context, id string) (biz.Tag, error) {
 	// TODO database operations
 
-	return nil
+	return biz.Tag{}, nil
 }
+
 // ListTags is
-func (d *TagsRepoImpl) ListTags(ctx context.Context) error {
+func (d *TagsRepoImpl) ListTags(ctx context.Context,
+	filter *biz.ListTagsFilter) ([]biz.Tag, error) {
 	// TODO database operations
 
-	return nil
+	return nil, nil
 }
