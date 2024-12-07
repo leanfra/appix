0f254f65a9e917d7fb9ea595099f2de962750e67
aggong <uniwolf@163.com>
2024-12-07 09:38:40 +0800

    feature impletment features biz code

--- internal/biz/features.go
+++ internal/biz/features.go
@@ -2,6 +2,7 @@ package biz
 
 import (
 	"context"
+	"fmt"
 )
 
 type FeaturesRepo interface {
@@ -22,28 +23,53 @@ func NewFeaturesUsecase(repo FeaturesRepo) *FeaturesUsecase {
 	}
 }
 
+func (s *FeaturesUsecase) validateFeatures(isNew bool, features []Feature) error {
+	for _, f := range features {
+		if err := f.Validate(isNew); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // CreateFeatures is
 func (s *FeaturesUsecase) CreateFeatures(ctx context.Context, features []Feature) error {
+	if err := s.validateFeatures(true, features); err != nil {
+		return err
+	}
 	return s.repo.CreateFeatures(ctx, features)
 }
 
 // UpdateFeatures is
 func (s *FeaturesUsecase) UpdateFeatures(ctx context.Context, features []Feature) error {
+	if err := s.validateFeatures(false, features); err != nil {
+		return err
+	}
 	return s.repo.UpdateFeatures(ctx, features)
 }
 
 // DeleteFeatures is
 func (s *FeaturesUsecase) DeleteFeatures(ctx context.Context, ids []string) error {
+	if len(ids) == 0 {
+		return fmt.Errorf("EmptyIds")
+	}
 	return s.repo.DeleteFeatures(ctx, ids)
 }
 
 // GetFeatures is
 func (s *FeaturesUsecase) GetFeatures(ctx context.Context, id string) (Feature, error) {
+	if len(id) == 0 {
+		return Feature{}, fmt.Errorf("EmptyId")
+	}
 	return s.repo.GetFeatures(ctx, id)
 }
 
 // ListFeatures is
 func (s *FeaturesUsecase) ListFeatures(ctx context.Context,
 	filter *ListFeaturesFilter) ([]Feature, error) {
+
+	if err := filter.Validate(); err != nil {
+		return nil, err
+	}
 	return s.repo.ListFeatures(ctx, filter)
 }
--- internal/biz/features_models.go
+++ internal/biz/features_models.go
@@ -1,18 +1,51 @@
 package biz
 
+import "fmt"
 
 type Feature struct {
-	Id string
-	Name string
-	Value string 
-}
-type FeatureFilter struct {
-	Name string
-	Value string 
-}
-type ListFeaturesFilter struct {
-	Page int64
-	PageSize int64
-	Filters  []FeatureFilter 
+	Id    string
+	Name  string
+	Value string
 }
 
+type FeatureFilter struct {
+	Name  string
+	Value string
+}
+
+type ListFeaturesFilter struct {
+	Page     int64
+	PageSize int64
+	Filters  []FeatureFilter
+}
+
+func (f *Feature) Validate(isNew bool) error {
+	if len(f.Name) == 0 || len(f.Value) == 0 {
+		return fmt.Errorf("InvalidNameValue")
+	}
+	if !isNew {
+		if len(f.Id) == 0 {
+			return fmt.Errorf("InvalidId")
+		}
+	}
+	return nil
+}
+
+func (ff *FeatureFilter) Validate() error {
+	if len(ff.Name) == 0 || len(ff.Value) == 0 {
+		return fmt.Errorf("InvalidFeatureFilterNameValue")
+	}
+	return nil
+}
+
+func (lf *ListFeaturesFilter) Validate() error {
+	if lf.Page < 0 || lf.PageSize < 0 {
+		return fmt.Errorf("InvalidPageSize")
+	}
+	for _, f := range lf.Filters {
+		if err := f.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
